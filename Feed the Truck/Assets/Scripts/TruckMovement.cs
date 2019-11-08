3770afac00dc4f2ac825e77669f8ffadba5484ef
Lucas Granberg <lucgr483@student.liu.se>
2019-11-08 21:50:06 +0100

    Basic truck with movement script

---
+++ Feed the Truck/Assets/Scripts/TruckMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    public int speed;
+    private Rigidbody2D rb;
+
+    public bool m_FacingRight { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(1,0) * speed);
+
+        if (speed < 0)
+        {
+            m_FacingRight = false;
+            transform.localScale = transform.localScale * -1;
+        }
+    }
+}
