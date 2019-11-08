5638b64c3482d8b28b1bd54bda830e7dedf8bbcc
Lucas Granberg <lucgr483@student.liu.se>
2019-11-08 22:19:58 +0100

    Grafik för truck

--- Feed the Truck/Assets/Scripts/TruckMovement.cs
+++ Feed the Truck/Assets/Scripts/TruckMovement.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class TruckMovement : MonoBehaviour
 {
+    public bool facingRight { get; set; }
     public int speed;
-    private Rigidbody2D rb;
 
-    public bool m_FacingRight { get; private set; }
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector2(1,0) * speed);
+        rb.AddForce(new Vector2(1,0.01f) * speed);
 
         if (speed < 0)
         {
-            m_FacingRight = false;
-            transform.localScale = transform.localScale * -1;
+            Debug.Log("flipping");
+            spriteRenderer.flipX = false;
         }
+        
     }
 }
