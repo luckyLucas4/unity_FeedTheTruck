6151fd92042ed7f14918db0d84fb3239e5012da8
Lucas Granberg <lucgr483@student.liu.se>
2019-11-09 11:38:31 +0100

    Cinemachine, compostfeeding

---
+++ Feed the Truck/Assets/FeedTruck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedTruck : MonoBehaviour
+{
+    public TruckMovement truckScript;
+    public int compost;
+    public Text compostText;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        compostText.text = "Compost: " + compost.ToString();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.GetComponent<TruckMovement>() && Input.GetKeyDown(KeyCode.E))
+        {
+            compost -= truckScript.Feeding(compost);
+        }
+    }
+}
